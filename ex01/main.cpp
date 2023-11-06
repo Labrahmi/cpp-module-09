54896d873b9a58f4b2c1e0bc174072ad2ebb8af8
youssef labrahmi <ylabrahm@c2r6p8.1337.ma>
2023-11-06 16:20:29 +0100

    last RPN

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ylabrahm <ylabrahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 15:36:13 by ylabrahm          #+#    #+#             */
-/*   Updated: 2023/11/04 16:41:38 by ylabrahm         ###   ########.fr       */
+/*   Updated: 2023/11/05 10:13:28 by ylabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,71 +15,66 @@
 #include <string>
 #include <sstream>
 
+void ft_err()
+{
+    std::cout << "Error" << std::endl;
+    exit(1);
+}
+
+void rpn_op(std::stack<int> &numbers, std::string &token)
+{
+    int x1, x2;
+    if (numbers.size() < 2)
+    {
+        std::cout << "Error" << std::endl;
+        exit(1);
+    }
+    x2 = numbers.top();
+    numbers.pop();
+    x1 = numbers.top();
+    numbers.pop();
+    if (token == "+")
+        numbers.push(x1 + x2);
+    else if (token == "-")
+        numbers.push(x1 - x2);
+    else if (token == "*")
+        numbers.push(x1 * x2);
+    else if (token == "/")
+    {
+        if (x2 == 0)
+            ft_err();
+        numbers.push(x1 / x2);
+    }
+    else
+        ft_err();
+}
+
 int main(int argc, char const *argv[])
 {
     if (argc > 1)
     {
         std::stack<int> numbers;
         std::string token;
-        std::string input(argv[1]);
-        std::istringstream iss(input);
+        std::istringstream iss(argv[1]);
+        int num = 0;
         while (iss >> token)
         {
-            if (isdigit(token[0]) || (token.length() > 1 && isdigit(token[1])))
-            {
-                int num;
-                std::istringstream(token) >> num;
-                numbers.push(num);
-            }
+            if (token.length() != 1)
+                ft_err();
             else
             {
-                if (numbers.size() < 2)
+                if (isdigit(token[0]))
                 {
-                    std::cout << "Error" << std::endl;
-                    return (1);
-                }
-                int x2 = numbers.top();
-                numbers.pop();
-                int x1 = numbers.top();
-                numbers.pop();
-
-                if (token == "+")
-                {
-                    numbers.push(x1 + x2);
-                }
-                else if (token == "-")
-                {
-                    numbers.push(x1 - x2);
-                }
-                else if (token == "*")
-                {
-                    numbers.push(x1 * x2);
-                }
-                else if (token == "/")
-                {
-                    if (x2 == 0)
-                    {
-                        std::cerr << "Division by zero is not allowed." << std::endl;
-                        return (1);
-                    }
-                    numbers.push(x1 / x2);
+                    std::istringstream(token) >> num;
+                    numbers.push(num);
                 }
                 else
-                {
-                    std::cerr << "Invalid operation: " << token << std::endl;
-                    return (1);
-                }
+                    rpn_op(numbers, token);
             }
         }
-
         if (numbers.size() != 1)
-        {
-            std::cerr << "Invalid RPN expression. More than one result." << std::endl;
-            return (1);
-        }
-
-        int result = numbers.top();
-        std::cout << result << std::endl;
+            ft_err();
+        std::cout << numbers.top() << std::endl;
     }
     return 0;
 }
